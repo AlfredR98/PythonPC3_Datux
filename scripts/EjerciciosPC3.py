0c7d4f7b3226844c02c094b31342d0db9a87ff6c
Alfred <48213244+AlfredR98@users.noreply.github.com>
2023-12-02 01:26:52 -0500

    Resuelto Ejercicios completos

--- scripts/EjerciciosPC3.py
+++ scripts/EjerciciosPC3.py
@@ -1,7 +1,6 @@
 '''
 Ejercicio 2
 '''
-'''
 import math
 
 class Circulo:
@@ -18,9 +17,7 @@ circulo = Circulo(8)
 areaC = circulo.calcular_area()
 print(f'El area de {circulo.radio} es: {areaC:.2f}')
 '''
-'''
 Ejercicio 3
-'''
 '''
 #crear la clase Catálogo y producto
 class Producto:
@@ -56,9 +53,7 @@ while i <= cantidad:
 
 c.mostrar_productos()
 '''
-'''
 Ejercicio 4
-'''
 '''
 from main import *
 
@@ -77,11 +72,9 @@ try:
          print('Acaba de salir del Menú, Nos vemos!')
 
 except Exception as e:
-    print(e)
-'''   
+    print(e) 
 '''
 Ejercicio 5
-'''
 '''
 class Producto:
     def __init__(self, nombre, codigo):
@@ -107,13 +100,10 @@ while True:
         p = Producto(nom,cod)
         print(p)
         break
-'''
+
 '''
 Ejercicio 6
 '''
-#para la pregunta 4 al importar la funciones usar el manejo de errores (try,except) 
-# y en las sentencias de “ else “ imprimir la ruta del directorio de trabajo os.getcwd() 
-# (previamente importar el modulo os < import os> y en la sentencia finally imprimir “proceso terminado”
 from main import *
 import os
 
@@ -137,3 +127,73 @@ else:
     print('El punto del directorio es: ',os.getcwd())
 finally:
     print('“Proceso terminado"')
+'''
+Ejercicio 7
+'''
+class Phone:
+
+    def __init__(self,nombreContacto,apellidoContacto,celContacto):
+        self.nombreContacto = nombreContacto
+        self.apellidoContacto = apellidoContacto
+        self.celContacto = celContacto
+        self.contacto = []
+      
+    def agregarContacto(self):
+        self.contacto.append(self.nombreContacto)
+        self.contacto.append(self.apellidoContacto)
+        self.contacto.append(self.celContacto)
+    
+    
+    def llamar(self):
+        try:
+            ingresarC = input('\nIngrese el nombre y apellido del contacto que desea llamar: ').lower()
+            primerN, apellido = ingresarC.split(' ')
+            contactoLlamar = primerN+''+apellido
+        
+            if contactoLlamar == self.nombreContacto+''+self.apellidoContacto:
+                return print(f'Llamando a ... {self.nombreContacto} {self.apellidoContacto}')
+            else:
+                return print('No existe dentro de sus contactos')
+        except Exception as e:
+            print(e)
+        
+    def bloquearContacto(self):
+        bloquearC = input('\n¿Cual es el nombre del contacto que quiere bloquear?: ')
+
+        if bloquearC in self.contacto:
+            self.bloquearContacto = True
+            print(f'Su contacto {self.nombreContacto} con el número {self.celContacto} ha sido bloqueado')
+        else: 
+            print('No existe dentro de sus contactos')
+                
+nomC = input('Ingrese el Nombre del contacto: ').lower()
+apeC = input('Ingrese el Apellido paterno del contacto: ').lower()
+celC = input('Ingrese el Número celular del contacto: ')  
+
+p = Phone(nomC,apeC,celC)
+p.agregarContacto()
+p.llamar()
+p.bloquearContacto()
+'''
+Ejercicio 8
+'''
+class Persona:
+
+    def __init__(self):
+        try: 
+            self.nombres = input('Ingrese su nombre completo: ').upper()
+            self.edad = int(input('Ingrese su edad: '))
+            self.sexo = input('Ingrese su sexo: ').upper()
+            self.dni = input('Ingrese su dni: ')
+            self.fecha_nacimiento = input('Ingrese su fecha de nacimiento: ')
+        except Exception as e:
+            print(e)
+        
+    def mostrar(self):
+        print(self)
+    
+    def __str__(self):
+        return 'Su nombre es: {}, su edad es: ({}), su sexo es: "{}", su DNI es: "{}" y su fecha de nacimiento es: "{}"'.format(self.nombres,self.edad,self.sexo,self.dni,self.fecha_nacimiento)
+
+p = Persona()
+p.mostrar()
--- scripts/main.py
+++ scripts/main.py
@@ -4,6 +4,9 @@ def fdividir(n1,n2):
 def menu():
     print('Menú:\n1. Dividir números\n2. Salir')
 
+'''
+Ejercicio 1
+'''
 
 if __name__ =='__main__':
     print('Ejecutar los demás problemas')
